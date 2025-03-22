0b562047182b8f4866fc22aed7e38792bb658221
Billazeezdinho <billazeezdinho@gmail.com>
2025-03-22 23:56:19 +1000

    dev

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
-
 const express = require('express');
-
 const router = require('./router/weatherRouter')
 const PORT = process.env.PORT;
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express()
 
 app.use(express.json());
-app.use('/api/v1', router)
+app.use(cors());
+app.use(morgan('dev'));
+app.use('/api/v1', router);
 
 app.listen(PORT, ()=>{
    console.log( `Server is listening to PORT ${PORT}`)
