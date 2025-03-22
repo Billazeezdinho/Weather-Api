5cb87bd053cb6de4dcc0a6f5449ceb23b5c31384
Billazeezdinho <billazeezdinho@gmail.com>
2025-03-23 06:16:02 +1000

    cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const morgan = require('morgan');
 const app = express()
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(morgan('dev'));
 app.use('/api/v1', router);
 
